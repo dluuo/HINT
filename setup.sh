d5128df4cb86ba3c2aa13c1a1d76b17579fcc6a5
David Luo <davidluogolf@gmail.com>
2023-05-12 12:07:26 +0200

    updated setup.sh with profhit dependencies

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate hint
 
 #install dependencies
-pip install datasetsforecast
-pip install statsforecast
+pip install datasetsforecast statsforecast
 pip install git+https://github.com/Nixtla/neuralforecast.git
 pip install git+https://github.com/Nixtla/hierarchicalforecast.git
+
+#PROFHiT dependency
+pip install properscoring
 
 conda deactivate
